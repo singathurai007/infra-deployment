b803eaa8835c84db2a8615a1510cde9a470ceb4b
singathurai007 <cmsingathurai@gmail.com>
2026-08-24 12:10:55 +0000

    Add Terraform EC2 infrastructure

--- main.tf
+++ main.tf
@@ -11,8 +11,49 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-output "aws_account_id" {
-  value = data.aws_caller_identity.current.account_id
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_instance" "dev_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  subnet_id = data.aws_subnets.default.ids[0]
+
+  tags = {
+    Name        = "terraform-dev-server"
+    Environment = "dev"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.dev_server.id
+}
+
+output "public_ip" {
+  value = aws_instance.dev_server.public_ip
 }
